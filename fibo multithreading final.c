cdb4474760587b970b0fd9de625e7e12ce1870b3
vishu3499 <38154578+vishu3499@users.noreply.github.com>
2018-04-11 22:41:36 +0530

    Add files via upload

---
+++ fibo multithreading final.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+int storage[100];
+int count;
+void *gen()
+{
+    int n,a=0,b=1,c=2,d;
+    printf("Enter Term: ");
+    scanf("%d", &n);
+    count=n;
+	storage[0]=a;
+	storage[1]=b;
+	for(c=2;c<n;c++)
+	{
+		d=a+b;
+    	storage[c]=d;
+    	a=b;
+    	b=d;
+	}	
+}
+void *fibo()
+{
+	
+	int i;
+	pthread_t child;
+    pthread_create( &child, NULL, gen, NULL);
+    pthread_join(child, NULL);
+    printf("\nFibo Series is:\n");
+    for(i=0;i<count;i++)
+	{
+		printf("%d\t",storage[i]);
+	} 
+}
+
+int main()
+{
+    pthread_t parent;
+    pthread_create( &parent, NULL, fibo, NULL);
+    pthread_join( parent, NULL);
+    printf ("\n");
+    return 0;
+}
+
